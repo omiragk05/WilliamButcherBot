86269c1d5c007a70fbbc2d9f257f6154dbba4ab9
omira geevin <92523621+omiragk05@users.noreply.github.com>
2022-07-01 08:45:47 -0700

    Update regex.py

--- wbb/modules/regex.py
+++ wbb/modules/regex.py
@@ -7,8 +7,8 @@ from pyrogram import filters
 from wbb import app
 from wbb.utils.filter_groups import regex_group
 
-__MODULE__ = "Sed"
-__HELP__ = "**Usage:**\ns/foo/bar"
+###__MODULE__ = "Sed"
+# __HELP__ = "**Usage:**\ns/foo/bar"
 
 DELIMITERS = ("/", ":", "|", "_")
 
